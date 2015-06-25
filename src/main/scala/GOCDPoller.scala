9f8682e7af8b0e6fa04979823a20b7ff52fd837c
tamizhgeek <tamizhgeek@gmail.com>
2015-06-25 21:43:06 +0530

    a basic poller for gocd scheduled tasks. This maintains a history of last 5 polls. when all of the last 5 polls have a postive backlog, add a agent job to task queue

--- src/main/scala/GOCDPoller.scala
+++ src/main/scala/GOCDPoller.scala
@@ -1,9 +1,30 @@
 package com.indix.mesos
 
+
+import scalaj.http._
+
 case class GOCDPoller(server: String, user: String, password: String) {
 
-  def poll() = {
+  val authToken = Base64.encode(s"Basic ${user}:${password}".getBytes)
 
+  var responseHistory: List[Int] = List.empty
+
+  def poll() = {
+    val response: HttpResponse[String] = Http(server).header("Authorization", authToken).asString
+    val responseXml = scala.xml.XML.loadString(response.body)
+    (responseXml \ "scheduled").size
+  }
+
+  def pollAndAddTask = {
+    val scheduled : List[String] = poll
+    responseHistory = responseHistory ++ scheduled.size
+    if(responseHistory.size > 5)
+      if(!responseHistory.exists(x => x <= 1)) {
+        TaskQueue.enqueue(GoTask("./install_go_agent.sh", "", "https://raw.githubusercontent.com/ind9/gocd-mesos/master/bin/install_goagent.sh"))
+        responseHistory = List.empty
+      }
+      else
+        responseHistory = responseHistory.drop(0)
   }
 
 }
