1a50b5d9ffedf4eed52aa82486be9ad195978f41
Sriram <sriram@indix.com>
2015-06-26 08:07:25 +0530

    Splitting out the resource class as separate file from Scheduler

--- src/main/scala/com/indix/mesos/Resources.scala
+++ src/main/scala/com/indix/mesos/Resources.scala
@@ -1,5 +1,28 @@
 package com.indix.mesos
 
-class Resources {
+import org.apache.mesos.Protos.Offer
+import scala.collection.JavaConverters._
 
+
+case class Resources(cpus: Double, memory: Double, disk: Double) {
+  def canSatisfy(another: Resources): Boolean = {
+    this.cpus >= another.cpus && this.memory >= another.memory
+  }
+}
+object Resources {
+  def apply(task: GoTask): Resources =  {
+    new Resources(task.resource.cpu,
+      task.resource.memory,
+      task.resource.disk)
+  }
+
+
+
+  def apply(offer: Offer): Resources = {
+    val resources = offer.getResourcesList.asScala
+    new Resources(
+      resources.find(_.getName == "cpus").map(x => x.getScalar.getValue).getOrElse(1),
+      resources.find(_.getName == "mem").map(x => x.getScalar.getValue).getOrElse(256),
+      resources.find(_.getName == "disk").map(x => x.getScalar.getValue).getOrElse(0))
+  }
 }
